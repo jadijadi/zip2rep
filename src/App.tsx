707a183e6cbb890822446879e855287872bbad06
Jadi <jadijadi@gmail.com>
2026-01-15 15:55:04 -0800

    changing MP with Representative for more inclusion

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ function App() {
                 disabled={loading || !country || !postalCode}
                 className="w-full bg-indigo-600 text-white py-3 px-6 rounded-lg font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-gray-400 disabled:cursor-not-allowed transition-colors"
               >
-                {loading ? 'Looking up...' : 'Find My MP'}
+                {loading ? 'Looking up...' : 'Find My Representative'}
               </button>
             </div>
           </form>
